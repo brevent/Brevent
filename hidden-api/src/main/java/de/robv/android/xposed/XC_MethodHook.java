aef894dcf2892e9a0f396f696354f48b8c41fcfc
Liu DongMiao <liudongmiao@gmail.com>
2017-02-19 19:56:23 +0800

    try disable xposed

---
+++ hidden-api/src/main/java/de/robv/android/xposed/XC_MethodHook.java
@@ -0,0 +1,8 @@
+package de.robv.android.xposed;
+
+/**
+ * Created by thom on 2017/2/18.
+ */
+
+public abstract class XC_MethodHook {
+}
---
+++ hidden-api/src/main/java/de/robv/android/xposed/XposedBridge.java
@@ -0,0 +1,25 @@
+package de.robv.android.xposed;
+
+import java.lang.reflect.Member;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by thom on 2017/2/18.
+ */
+public class XposedBridge {
+
+    /*package*/ static boolean disableHooks = false;
+
+    private static final Map<Member, CopyOnWriteSortedSet<XC_MethodHook>> sHookedMethodCallbacks = new HashMap<>();
+
+    /** @hide */
+    public static final class CopyOnWriteSortedSet<E> {
+
+        public Object[] getSnapshot() {
+            throw new UnsupportedOperationException();
+        }
+
+    }
+
+}
