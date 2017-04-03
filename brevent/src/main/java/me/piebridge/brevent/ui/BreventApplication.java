4ae9fafeb86acdcff66b398b5d656977c2fae50b
Liu DongMiao <liudongmiao@gmail.com>
2017-04-04 07:16:42 +0800

    version 0.9.6
    close #354, #355, #356, #359, #360
    
    only brevent-server changed

--- brevent/src/main/java/me/piebridge/brevent/ui/BreventApplication.java
+++ brevent/src/main/java/me/piebridge/brevent/ui/BreventApplication.java
@@ -3,7 +3,6 @@ package me.piebridge.brevent.ui;
 import android.app.Application;
 import android.content.res.Resources;
 import android.os.Build;
-import android.os.Environment;
 
 import java.io.File;
 import java.io.FileOutputStream;
