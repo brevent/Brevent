42aad11476976fd67fb999322baf3b178576f6f1
Liu DongMiao <liudongmiao@gmail.com>
2017-04-03 18:50:53 +0800

    support abnormal rom, close #350

--- brevent/src/main/res/raw/brevent.sh
+++ brevent/src/main/res/raw/brevent.sh
@@ -15,6 +15,13 @@ else
     else
         rm -rf $brevent
         cp $path $brevent
+        if echo $path | grep -q -v 64; then
+            rm -rf /data/local/tmp/app_process
+            if [ -x /system/bin/app_process32 ]; then
+                ln -s /system/bin/app_process32 /data/local/tmp/app_process
+                export PATH=/data/local/tmp:$PATH
+            fi
+        fi
         exec $brevent
         exit 0
     fi
