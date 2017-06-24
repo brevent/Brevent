d059bf21847758f36e5643c89fd677e912ba055c
Liu DongMiao <liudongmiao@gmail.com>
2017-06-24 08:49:57 +0800

    support wallpaper manager in n-

--- hidden-api/src/main/java/android/app/IWallpaperManager.java
+++ hidden-api/src/main/java/android/app/IWallpaperManager.java
@@ -1,14 +1,23 @@
 package android.app;
 
+import android.os.Build;
 import android.os.IBinder;
 import android.os.RemoteException;
+import android.support.annotation.RequiresApi;
 
 /**
  * Created by thom on 2017/6/24.
  */
 public interface IWallpaperManager {
 
+    @RequiresApi(Build.VERSION_CODES.N_MR1)
     WallpaperInfo getWallpaperInfo(int userId) throws RemoteException;
+
+    /**
+     * deprecated since 7.1
+     */
+    @Deprecated
+    WallpaperInfo getWallpaperInfo() throws RemoteException;
 
     abstract class Stub {
 
