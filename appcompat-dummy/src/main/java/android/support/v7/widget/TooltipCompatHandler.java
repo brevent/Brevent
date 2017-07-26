34652c70d50c7d121526fe0f9df53e4ad68dab7c
Liu DongMiao <liudongmiao@gmail.com>
2017-07-26 11:03:44 +0800

    Add TooltipCompat for design 26
    - no tooltip compat

---
+++ appcompat-dummy/src/main/java/android/support/v7/widget/TooltipCompatHandler.java
@@ -0,0 +1,14 @@
+package android.support.v7.widget;
+
+import android.view.View;
+
+/**
+ * Created by thom on 2017/7/26.
+ */
+public class TooltipCompatHandler {
+
+    public static void setTooltipText(View view, CharSequence tooltipText) {
+
+    }
+
+}
