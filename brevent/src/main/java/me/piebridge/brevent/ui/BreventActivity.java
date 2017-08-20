b3e86a63792acdea310295bfdaef68552954f584
Liu DongMiao <liudongmiao@gmail.com>
2017-08-20 20:17:27 +0800

    only show paused for running apps

--- brevent/src/main/java/me/piebridge/brevent/ui/BreventActivity.java
+++ brevent/src/main/java/me/piebridge/brevent/ui/BreventActivity.java
@@ -755,7 +755,7 @@ public class BreventActivity extends Activity
         }
         if (BreventResponse.isAudio(status)) {
             return R.drawable.ic_audiotrack_black_24dp;
-        } else if (BreventResponse.isAudioPaused(status)) {
+        } else if (BreventResponse.isRunning(status) && BreventResponse.isAudioPaused(status)) {
             return R.drawable.ic_pause_circle_outline_black_24dp;
         } else if (isFavorite(packageName)) {
             return R.drawable.ic_favorite_border_black_24dp;
