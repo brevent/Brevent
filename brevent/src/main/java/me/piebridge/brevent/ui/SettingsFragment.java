301bcfd4f061af15ee664e2d7a9288698f46fd2a
Liu DongMiao <liudongmiao@gmail.com>
2017-09-26 22:49:54 +0800

    remove unused import

--- brevent/src/main/java/me/piebridge/brevent/ui/SettingsFragment.java
+++ brevent/src/main/java/me/piebridge/brevent/ui/SettingsFragment.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.SystemProperties;
 import android.preference.Preference;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
